6438ebf9c96a774abe36eda15581d89ffe44d739
pannnn3310-ux <pannnn3310@gmail.com>
2026-01-08 14:25:05 +0800

    重整拉霸條件

--- script.js
+++ script.js
@@ -309,7 +309,7 @@ dropdownItems.forEach(item => {
       specialPrizeInput.style.display = "inline-block";
       specialPrizeInput2.style.display = "none";
       specialPrizeAmountInput.style.display = "block";
-      stickChang.style.top = "15%";
+      setStickOffset(110);
       specialBalanceBtn.style.display = "block";
     } else if (value === "10"){
       specialBalanceInput.style.display = "none";
@@ -318,7 +318,7 @@ dropdownItems.forEach(item => {
       specialPrizeInput.style.display = "none";
       specialBalanceBtn.style.display = "none";
       specialPrizeAmountInput.style.display = "block";
-      stickChang.style.top = "6%";
+      setStickOffset(40);
     } else if (value === "11") {  // 額外加碼獎
       specialBalanceInput.style.display = "none";
       specialPrizeContainer.style.display = "block";
@@ -326,11 +326,11 @@ dropdownItems.forEach(item => {
       specialPrizeInput2.style.display = "none";
       specialPrizeAmountInput.style.display = "block";
       specialBalanceBtn.style.display = "block";
-      stickChang.style.top = "5%";
+      setStickOffset(35);
     } else {
       specialPrizeContainer.style.display = "none";
       specialBalanceBtn.style.display = "none";
-      stickChang.style.top = "0%";
+      setStickOffset(0);
     };
 
   });
@@ -1219,3 +1219,8 @@ clearAllBtn.addEventListener('click', () => {
   };
 });
 
+//拉霸高度用
+
+function setStickOffset(px) {
+  stickChang.style.transform = `translateY(calc(0% + ${px}px))`;
+}
