a52d05e1e1afe080754396c3c528a865b587ea54
pannnn3310-ux <pannnn3310@gmail.com>
2026-01-27 14:12:09 +0800

    修正細節

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let allNamesById = [];
 const STORAGE_KEY = 'lucky-draw-formal_2026';
 const scrollArea = document.querySelector('#scroll-area');
 const dropdownButton = document.querySelector('#prize-btn');
-const prizeText = document.querySelector
-  ('#prize-btn');
+const prizeText = dropdownButton;
 const lever = document.querySelector('.lever');
 const dropdownItems = document.querySelectorAll('.dropdown-item');
 const specialPrizeContainer = document.querySelector('#special-prize-container');
@@ -1090,7 +1089,7 @@ function handleWinnerText(winner) {
 
   const isSharePrize = prizeValue === "9"
 
-  const shareToId = isSharePrize
+  let shareToId = isSharePrize
   ? specialPrizeInput.dataset.id || null
   : null;
 
@@ -1366,7 +1365,7 @@ function populateSpecialPrizeList2() {
       div.style.cursor = 'pointer';
 
       div.addEventListener('click', () => {
-        specialPrizeInput.dataset.id = div.dataset.id; // 存工號
+        specialPrizeInput2.dataset.id = div.dataset.id; // 存工號
         specialPrizeInput2.value = div.textContent;
         specialPrizeDropdown2.style.display = 'none';
       });
