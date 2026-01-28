f16f4076998cb8001a9dc845e44c2eccefa75e4c
pannnn3310-ux <pannnn3310@gmail.com>
2026-01-28 15:18:34 +0800

    1.5

--- script.js
+++ script.js
@@ -599,9 +599,9 @@ async function doDraw() {
     ];
   } else if (noDelayPrizes12.includes(prizeValue)) {
     reelDurations = [
-      800 + fullRounds * 100,
-      800 + fullRounds * 100 ,
-      800 + fullRounds * 100
+      800 + fullRounds * 30,
+      800 + fullRounds * 30 ,
+      800 + fullRounds * 30
     ];
   } else if (noDelayPrizes56.includes(prizeValue)) {
     reelDurations = [
